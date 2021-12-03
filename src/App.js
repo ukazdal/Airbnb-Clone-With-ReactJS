f147221b12dd188ee6a3a5632a84220290311e1e
ukazdal <ibrahimufukkazdal@gmail.com>
2021-12-03 14:30:18 +0300

    Footer-Update-v1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Header />
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <SearchPage />
+        </Switch>
         <Footer />
       </Router>
     </div>
---
+++ src/components/SearchPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchPage.css";
+
+function SearchPage() {
+  return (
+    <div className="searchpage">
+      <h1>Searcpage</h1>
+    </div>
+  );
+}
+
+export default SearchPage;
