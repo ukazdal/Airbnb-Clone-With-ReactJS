13487aa3e7d2cbba84c22fd4a1b3c1ef5d80cfe5
ukazdal <ibrahimufukkazdal@gmail.com>
2021-12-07 11:26:02 +0300

    home-message-update

--- src/components/pages/homemessage.js
+++ src/components/pages/homemessage.js
@@ -1,58 +1,88 @@
 import React from "react";
 import "./homemessage.css";
-import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
-import ForwardToInboxIcon from "@material-ui/icons/Forward";
-import BusinessIcon from "@material-ui/icons/Business";
+// import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+// import ForwardToInboxIcon from "@material-ui/icons/Forward";
+// import BusinessIcon from "@material-ui/icons/Business";
+// import CastConnected from "@material-ui/icons/CastConnected";
 
 function homemessage() {
   return (
-    <div className="contact">
-      <div className="content">
-        <h2>Contact Us</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-          nobis?
-        </p>
-      </div>
-      <div className="container">
-        <div className="contact__info">
-          <div className="box">
-            <div className="icon">
-              <div className="text">
-                <h3>
-                  Address <BusinessIcon />
-                </h3>
-                <p>
-                  43325 USA Camp, <br /> New York Usa <br />
-                  559944
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="box">
-            <div className="icon">
-              <div className="text">
-                <h3>
-                  Phone <PhoneAndroidIcon />
-                </h3>
-                <p>0090-532-154-45-01</p>
-              </div>
-            </div>
-          </div>
-          <div className="box">
-            <div className="icon">
-              <div className="text">
-                <h3>
-                  Email <ForwardToInboxIcon />
-                </h3>
-                <p>info@ufukkazdal.com</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="homemessage">
+      <h2>To contact the host, simply send us an e-mail.</h2>
     </div>
   );
 }
 
 export default homemessage;
+
+// <div className="contact">
+// <div className="content">
+// <h2>Contact Us</h2>
+// <p>
+//   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, esse!
+//   Expedita praesentium recusandae nostrum necessitatibus, amet alias.
+//   Pariatur, temporibus similique?
+// </p>
+// <CastConnected />
+// </div>
+// <div className="container">
+// <div className="contact__info">
+//   <div className="box">
+//     <div className="icon">
+//       <BusinessIcon />
+//     </div>
+//     <div className="text">
+//       <h3>Address</h3>
+//       <p>
+//         43325 USA Camp, <br /> New York Usa <br />
+//         559944
+//       </p>
+//     </div>
+//   </div>
+//   <div className="box">
+//     <div className="icon">
+//       <PhoneAndroidIcon />
+//     </div>
+//     <div className="text">
+//       <h3>Phone</h3>
+//       <p>0090-532-154-45-01</p>
+//     </div>
+//   </div>
+//   <div className="box">
+//     <div className="icon">
+//       <ForwardToInboxIcon />
+//     </div>
+//     <div className="text">
+//       <h3>Email</h3>
+//       <p>info@ufukkazdal.com</p>
+//     </div>
+//   </div>
+// </div>
+// <div className="contact__form">
+//   <form>
+//     <h2>Send Message</h2>
+//     <div className="input__box">
+//       <input type="text" name="" required="required" />
+//       <span>Full Name</span>
+//     </div>
+//     <div className="input__box">
+//       <input type="text" name="" required="required" />
+//       <span>Email</span>
+//     </div>
+//     <div className="input__box">
+//       <textarea
+//         required="required"
+//         name=""
+//         id=""
+//         cols="30"
+//         rows="10"
+//       ></textarea>
+//       <span>Tyoe your Message...</span>
+//     </div>
+//     <div className="input__box">
+//       <input type="submit" name="" value="Send" />
+//     </div>
+//   </form>
+// </div>
+// </div>
+// </div>
